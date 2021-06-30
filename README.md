a1de6d59cd3042a69c3b8e5f17f3a4ee5e450b9b
Collin McNeese <cmcneese@chef.io>
2021-06-30 10:42:21 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# policyfiles_from_env_files
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: policyfiles_from_env_files
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
